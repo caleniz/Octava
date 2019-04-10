3a95653662c7c68ddb78fe222843ae924dab5e05
Carlos <caleniz@alumnos.uai.cl>
2019-04-09 20:51:50 -0400

    Integración CloudKit Integración Firebase

--- Octava/ViewController.swift
+++ Octava/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-var caca = 0
+class ViewController: UITableViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EquiposMotorizados", for: indexPath)
+        return cell
+    }
 }
 
